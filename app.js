361673bd47cc2a9fea5f6ef2feadd195214080de
Mike Armando <mike.armando.m@gmail.com>
2022-07-08 19:50:53 -0700

    Updatet

---
+++ app.js
@@ -0,0 +1,5 @@
+const startBtn = document.getElementById("start-btn")
+const newCardBtn = document.getElementById("new-card-btn")
+
+let cards = document.getElementById("cards")
+let sum = document.getElementById("sum")
