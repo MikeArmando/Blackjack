41361cbbfe57cc9aeebbafe10f752cb963512f1c
Mike Armando <mike.armando.m@gmail.com>
2022-07-08 19:56:36 -0700

    Updatet

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const newCardBtn = document.getElementById("new-card-btn")
 
 let cards = document.getElementById("cards")
 let sum = document.getElementById("sum")
+
+let randomNum = Math.floor(Math.random() * 6) + 1
