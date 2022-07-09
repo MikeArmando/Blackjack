6a68f71ba20a8dcfb77038aafefce3d861de9b29
Mike Armando <mike.armando.m@gmail.com>
2022-07-09 05:44:14 -0700

    Updatet

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const newCardBtn = document.getElementById("new-card-btn")
 let cards = document.getElementById("cards")
 let sum = document.getElementById("sum")
 
-
 newCardBtn.addEventListener("click", function() {
-    let randomNum = Math.floor(Math.random() * 6) + 1
+    let randomNum = Math.floor(Math.random() * 13) + 1
     cards.textContent += " " + randomNum
-})
+    
+    sum.textContent += randomNum
 
+    if (randomNum === 1) {
+        return 11
+    } else if (randomNum > 10) {
+        return 10
+    } else {
+        return randomNum
+    }
+})
