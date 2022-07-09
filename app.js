37b23e34d334558988ba160cbb58b11b26d24444
Mike Armando <mike.armando.m@gmail.com>
2022-07-09 14:16:57 -0700

    Updatet

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const startBtn = document.getElementById("start-btn")
+// Grabs the "START" button and the "NEW CARD" button
+const startGameBtn = document.getElementById("start-btn")
 const newCardBtn = document.getElementById("new-card-btn")
+let gameText = document.getElementById("game-text")
 
-let cards = document.getElementById("cards")
-let sum = document.getElementById("sum")
+let cardsEl = document.getElementById("cards")
+let sumEl = document.getElementById("sum")
 
-newCardBtn.addEventListener("click", function() {
+let alive = false
+let blackjack = false
+
+let sum
+
+startGameBtn.addEventListener("click", function() {
+    renderGame()
+    gameText.textContent = "Want to draw another card?"
+})
+
+function getRandomCard() {
     let randomNum = Math.floor(Math.random() * 13) + 1
-    cards.textContent += " " + randomNum
-    
-    sum.textContent += randomNum
+    cardsEl.textContent += " " + randomNum
 
     if (randomNum === 1) {
         return 11
@@ -17,4 +27,21 @@ newCardBtn.addEventListener("click", function() {
     } else {
         return randomNum
     }
-})
+}
+
+function renderGame() {
+    let randomNum = Math.floor(Math.random() * 13) + 1
+    cardsEl.textContent += " " + randomNum
+
+    newCardBtn.addEventListener("click", function() {
+        getRandomCard()
+    })
+
+    if (randomNum === 1) {
+        return 11
+    } else if (randomNum > 10) {
+        return 10
+    } else {
+        return randomNum
+    }
+}
