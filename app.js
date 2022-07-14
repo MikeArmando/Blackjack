c0a396d98274dceb06a85e3dd764ce438023b09f
Mike Armando <mike.armando.m@gmail.com>
2022-07-14 13:17:01 -0700

    Updatet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Grabs the "START" button and the "NEW CARD" button
 const startGameBtn = document.getElementById("start-btn")
 const newCardBtn = document.getElementById("new-card-btn")
+
+// Grabs the text
 let gameText = document.getElementById("game-text")
 
 let cardsEl = document.getElementById("cards")
@@ -9,16 +11,28 @@ let sumEl = document.getElementById("sum")
 let alive = false
 let blackjack = false
 
-let sum
+let cards = []
 
-startGameBtn.addEventListener("click", function() {
+let sum = 0
+
+startGameBtn.addEventListener("click", function () {
+
     renderGame()
+    getRandomCard()
+    
     gameText.textContent = "Want to draw another card?"
 })
 
 function getRandomCard() {
     let randomNum = Math.floor(Math.random() * 13) + 1
     cardsEl.textContent += " " + randomNum
+
+    cards.push(randomNum)
+
+    for (let i = 0; i < cards.length; i++) {
+        sum += cards[i]
+    }
+
 
     if (randomNum === 1) {
         return 11
@@ -30,18 +44,7 @@ function getRandomCard() {
 }
 
 function renderGame() {
-    let randomNum = Math.floor(Math.random() * 13) + 1
-    cardsEl.textContent += " " + randomNum
-
-    newCardBtn.addEventListener("click", function() {
+    newCardBtn.addEventListener("click", function () {
         getRandomCard()
     })
-
-    if (randomNum === 1) {
-        return 11
-    } else if (randomNum > 10) {
-        return 10
-    } else {
-        return randomNum
-    }
 }
