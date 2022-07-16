fbc1eb9a0326c0b99a5ee116cb52f169ae3b03ff
Mike Armando <mike.armando.m@gmail.com>
2022-07-16 13:23:04 -0700

    Updatet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Grabs the "START" button and the "NEW CARD" button
 const startGameBtn = document.getElementById("start-btn")
 const newCardBtn = document.getElementById("new-card-btn")
+document.getElementById("new-card-btn").disabled = true
 
 // Grabs the text
 let gameText = document.getElementById("game-text")
@@ -11,28 +12,34 @@ let sumEl = document.getElementById("sum")
 let alive = false
 let blackjack = false
 
+let sum = 0
 let cards = []
 
-let sum = 0
 
 startGameBtn.addEventListener("click", function () {
+    document.getElementById("start-btn").disabled = true
+    document.getElementById("new-card-btn").disabled = false
 
-    renderGame()
-    getRandomCard()
-    
-    gameText.textContent = "Want to draw another card?"
+    alive = true
+
+    let fCard = renderGame()
+    let sCard = renderGame()
+
+    cards = [fCard, sCard]
+
+    sum = fCard + sCard
+
 })
 
-function getRandomCard() {
+newCardBtn.addEventListener("click", function () {
+    renderGame()
+})
+
+function renderGame() {
     let randomNum = Math.floor(Math.random() * 13) + 1
     cardsEl.textContent += " " + randomNum
 
-    cards.push(randomNum)
-
-    for (let i = 0; i < cards.length; i++) {
-        sum += cards[i]
-    }
-
+    sumEl.textContent = "Sum: " + sum
 
     if (randomNum === 1) {
         return 11
@@ -43,8 +50,9 @@ function getRandomCard() {
     }
 }
 
-function renderGame() {
-    newCardBtn.addEventListener("click", function () {
-        getRandomCard()
-    })
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
 }
